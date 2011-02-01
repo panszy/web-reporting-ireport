d2134cd95f44b9e1955540933a4dd78a9d151599
p.munandar.p@gmail.com <p.munandar.p@gmail.com@97ed8218-d293-66c7-5460-163862d83f46>
2011-02-01 09:11:43 +0000

    initial commit

---
+++ trunk/src/database/QueryExecutor.java
@@ -0,0 +1,69 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class QueryExecutor {
+	
+	private final String selectQueryById = "Select query,type from querylist where id = ?";
+	private String query;	
+	private int queryType;
+	
+	public QueryExecutor(int queryId) throws Exception {
+		Connection conn = Connector.getInstance().getConnection();
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(selectQueryById);						
+			ps.setInt(1, queryId);			
+			rs = ps.executeQuery();
+			this.query = rs.getString(1);
+			this.queryType = rs.getInt(2);
+		} finally {
+			if(rs != null)
+				rs.close();
+			if(ps != null)
+				ps.close();
+			Connector.getInstance().putConnection(conn);
+		}
+	}
+	
+	public ArrayList<String[]> executeQuery(Object[] parameters) throws Exception {
+		Connection conn = Connector.getInstance().getConnection();
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		try {
+			ps = conn.prepareStatement(query);
+			int counter = 1;
+			for(Object o: parameters)
+				ps.setObject(counter++, o);
+			if(queryType == 0){
+				rs = ps.executeQuery();
+				return convert(rs);
+			} else {
+				ps.executeUpdate();
+				return null;
+			}
+		} finally {
+			if(rs != null)
+				rs.close();
+			if(ps != null)
+				ps.close();
+			Connector.getInstance().putConnection(conn);
+		}				
+	}
+	
+	private ArrayList<String[]> convert(ResultSet rs) throws Exception {
+		ArrayList<String[]> result = new ArrayList<String[]>();
+		while(rs.next()){
+			String[] element = new String[rs.getFetchSize()];
+			for(int i = 1; i <= rs.getFetchSize(); i++)
+				element[i-1] = rs.getString(i);
+			result.add(element);
+		}
+		return result;
+	}
+
+}
