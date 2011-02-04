f6ff7e6592b2ea29a0ef6b92828ba10ce7daf78d
p.munandar.p@gmail.com <p.munandar.p@gmail.com@97ed8218-d293-66c7-5460-163862d83f46>
2011-02-04 09:06:01 +0000

    update running initial version

--- trunk/src/database/QueryExecutor.java
+++ trunk/src/database/QueryExecutor.java
@@ -4,42 +4,47 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import common.Util;
+import listener.Listener;
 
 public class QueryExecutor {
-	
-	private final String selectQueryById = "Select query,type from querylist where id = ?";
-	private String query;	
+
+	private final String selectQueryById = "select query,type from querylist where id = ?";
+	private String query;
 	private int queryType;
-	
+
 	public QueryExecutor(int queryId) throws Exception {
 		Connection conn = Connector.getInstance().getConnection();
 		ResultSet rs = null;
 		PreparedStatement ps = null;
 		try {
-			ps = conn.prepareStatement(selectQueryById);						
-			ps.setInt(1, queryId);			
+			ps = conn.prepareStatement(selectQueryById);
+			ps.setInt(1, queryId);
 			rs = ps.executeQuery();
-			this.query = rs.getString(1);
-			this.queryType = rs.getInt(2);
+			if (rs.next()) {
+				this.query = rs.getString(1);
+				this.queryType = rs.getInt(2);
+			}
 		} finally {
-			if(rs != null)
+			if (rs != null)
 				rs.close();
-			if(ps != null)
+			if (ps != null)
 				ps.close();
-			Connector.getInstance().putConnection(conn);
+			Connector.putConnection(conn);
 		}
 	}
-	
-	public ArrayList<String[]> executeQuery(Object[] parameters) throws Exception {
+
+	public ArrayList<String[]> executeQuery(Object[] parameters)
+			throws Exception {
 		Connection conn = Connector.getInstance().getConnection();
 		ResultSet rs = null;
 		PreparedStatement ps = null;
 		try {
 			ps = conn.prepareStatement(query);
 			int counter = 1;
-			for(Object o: parameters)
+			for (Object o : parameters)
 				ps.setObject(counter++, o);
-			if(queryType == 0){
+			if (queryType == 0) {
 				rs = ps.executeQuery();
 				return convert(rs);
 			} else {
@@ -47,28 +52,60 @@ public class QueryExecutor {
 				return null;
 			}
 		} finally {
-			if(rs != null)
+			if (rs != null)
 				rs.close();
-			if(ps != null)
+			if (ps != null)
 				ps.close();
-			Connector.getInstance().putConnection(conn);
-		}				
+			Connector.putConnection(conn);
+		}
 	}
-	
+
 	private ArrayList<String[]> convert(ResultSet rs) throws Exception {
 		ArrayList<String[]> result = new ArrayList<String[]>();
-		while(rs.next()){
-			String[] element = new String[rs.getFetchSize()];
-			for(int i = 1; i <= rs.getFetchSize(); i++)
-				element[i-1] = rs.getString(i);
+		while (rs.next()) {
+			String[] element = new String[rs.getMetaData().getColumnCount()];
+			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
+				element[i - 1] = rs.getString(i);
 			result.add(element);
 		}
 		return result;
 	}
 
-	public static void main(String[] args)  throws Exception {
-		new Connector("db2admin", "password", "localhost", 50000, 1, 10);
-		QueryExecutor q = new QueryExecutor(1);
-		System.out.println(q.executeQuery(new Object[0]));
-	}
+	public static void main(String[] args) throws Exception {
+		Listener listener = new Listener();
+		listener.start();
+		Thread.sleep(100);
+		
+		ArrayList<String[]> res = Util.sendQuery("localhost", 9876, 1, new String[0]);
+
+		for (String[] stringList : res) {
+			for (String string : stringList)
+				System.out.print(string + "|");
+			System.out.println("");
+		}
+		
+		res = Util.sendQuery("localhost", 9876, 1, new String[0]);
+
+		for (String[] stringList : res) {
+			for (String string : stringList)
+				System.out.print(string + "|");
+			System.out.println("");
+		}
+		
+		res = Util.sendQuery("localhost", 9876, 2, new String[0]);
+
+		for (String[] stringList : res) {
+			for (String string : stringList)
+				System.out.print(string + "|");
+			System.out.println("");
+		}
+		
+		res = Util.sendQuery("localhost", 9876, 4, new String[]{"3"});
+
+		for (String[] stringList : res) {
+			for (String string : stringList)
+				System.out.print(string + "|");
+			System.out.println("");
+		}
+	}	
 }
--- trunk/src/listener/Handler.java
+++ trunk/src/listener/Handler.java
@@ -1,62 +1,43 @@
 package listener;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 import database.QueryExecutor;
 
 public class Handler implements Runnable {
 	
-	private Socket s;
-	private String data = "";
-	private String result = "";
+	private Socket s;	
 	
 	public Handler (Socket s){
 		this.s = s;
 	}
 
 	public void run() {
-		DataInputStream dis = null;
-		DataOutputStream dos = null;
-		try {			
-			dis = new DataInputStream(s.getInputStream());
-			byte[] buffer = new byte[1024];  
-			int length = dis.read(buffer);
-			while(length == 1024){					
-				for(byte b : buffer)
-					data += (char)b;
-			}
-			// format input: idquery,paramter...<delimiter:ascii of 33>			
-			int indexOfComma = data.indexOf(",");
-			String idQuery = data.substring(0,indexOfComma);
-			StringTokenizer st = new StringTokenizer(data.substring(indexOfComma),""+(char)33);
-			Object[] o = new Object[st.countTokens()];
-			for(int i = 0; i < o.length ; i++)
-				o[i] = st.nextToken();
-			QueryExecutor query = new QueryExecutor(Integer.parseInt(idQuery));
-			ArrayList<String[]> tempResult = query.executeQuery(o);
-			// format output: columnn...<delimiter:ascii of 33>, row<delimiter:ascii of 44>
-			for(int i = 0; i < tempResult.size() ; i++)
-				for (int j = 0; j < tempResult.get(i).length ; j++){
-					if (j < tempResult.get(i).length - 1)
-						result+=tempResult.get(i)[j]+(char)33;
-					else
-						result+=tempResult.get(i)[j]+(char)44;
-				}				
-			dos = new DataOutputStream(s.getOutputStream());			
-			dos.write(result.getBytes());
+		ObjectInputStream ois = null;
+		ObjectOutputStream oos = null;
+		try {
+			// format input: ArrayList<Object> : first Object is queryId=int, second is parameters-String[]
+			ois = new ObjectInputStream(s.getInputStream());
+			@SuppressWarnings("unchecked")
+			ArrayList<Object> res = (ArrayList<Object>) ois.readObject();
+			QueryExecutor query = new QueryExecutor((Integer)(res.get(0)));
+			ArrayList<String[]> tempResult = query.executeQuery((String[])res.get(1));
+			// format output: ArrayList<String[]>			
+			oos = new ObjectOutputStream(s.getOutputStream());			
+			oos.writeObject(tempResult);
+			oos.flush();
 		} catch (Exception e) { 
 			e.printStackTrace();
 		} finally {
 			try {
-				if(dis != null)
-					dis.close();
-				if(dos != null)
-					dos.close();
+				if(ois != null)
+					ois.close();
+				if(oos != null)
+					oos.close();
 				s.close();
 			} catch (IOException e) {
 				e.printStackTrace();
